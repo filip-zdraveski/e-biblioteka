b9d6caf1784ea9f59e62a120c04e57a78ed01d47
Filip Zdraveski <filip.zdraveski@yahoo.com>
2019-08-08 13:05:31 +0200

    TestKlasa added

---
+++ E-biblioteka/Models/TestKlasa.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace E_biblioteka.Models
+{
+    public class TestKlasa
+    {
+        [Required]
+        private String nekoePole;
+    }
+}
