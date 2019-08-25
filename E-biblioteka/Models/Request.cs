549d273f1b74aa1d04f51efb8042c033a5ea6157
Филип Трајковски <trajkovskifilip7@gmail.com>
2019-08-25 17:30:51 +0200

    Request model and view for book requests created

---
+++ E-biblioteka/Models/Request.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace E_biblioteka.Models
+{
+    public class Request
+    {
+        [Display(Name = "Request ID")]
+        public long RequestId { get; set; }
+        [Required]
+        [StringLength(128)]
+        public string Title { get; set; }
+        [Required]
+        [StringLength(128)]
+        [Display(Name = "Author's Name")]
+        public string AuthorsName { get; set; }
+        public string Comment { get; set; }
+    }
+}
