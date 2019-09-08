89b2bc9c34f052a87612197ce0f160ebc89a1e7a
Филип Трајковски <trajkovskifilip7@gmail.com>
2019-09-08 12:16:33 +0200

    CreateFromRequest view created

--- E-biblioteka/Controllers/BooksController.cs
+++ E-biblioteka/Controllers/BooksController.cs
@@ -41,6 +41,16 @@ namespace E_biblioteka.Controllers
             return View();
         }
 
+        // GET: Books/Create
+        public ActionResult CreateFromRequest(Request request)
+        {
+            Book newBook = new Book
+            {
+                Name = request.Title
+            };
+            return View(newBook);
+        }
+
         // POST: Books/Create
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
