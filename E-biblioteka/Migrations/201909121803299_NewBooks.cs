9dde2a2a2d813ae27b0fd127a63cbb2dbe976b9c
Aida Masovic <aida_masovic@hotmail.com>
2019-09-12 22:09:23 +0200

    Table replaced with cards

---
+++ E-biblioteka/Migrations/201909121803299_NewBooks.cs
@@ -0,0 +1,16 @@
+namespace E_biblioteka.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class NewBooks : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
