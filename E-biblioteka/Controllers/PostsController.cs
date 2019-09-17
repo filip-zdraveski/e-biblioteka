ba2d738157314c734a34b99a6b9bf3bbba3af6bc
Антонио Аврамовиќ <Antonio.avramovic@gmail.com>
2019-09-17 21:51:43 +0200

    add post controller and forum model

---
+++ E-biblioteka/Controllers/PostsController.cs
@@ -0,0 +1,95 @@
+﻿using E_biblioteka.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace E_biblioteka.Controllers
+{
+    public class PostsController : Controller
+    {
+        ApplicationDbContext db = new ApplicationDbContext();
+        // GET: Post
+        /** Gives back the posts base on page
+         */
+        public ActionResult Index(int? page, string orderBy, int bookId)
+        {
+            return View();
+        }
+
+        // GET: Post/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        
+
+        // GET: Post/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Post/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Post/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Post/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Post/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Post/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ E-biblioteka/Models/Forum/Forum.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace E_biblioteka.Models.Forum
+{
+    public class Forum
+    {
+
+    }
+}
